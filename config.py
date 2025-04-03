ff877df68ace0f4e223daf82d18c91d15d0fb310
Heet Patel <hiheet04@gmail.com>
2025-04-03 17:55:44 +0530

    structure fixes, creating session for recording water_quantity still left

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ port = 8883
 topic = "python/mqtt"
 username = 'heet434'
 password = 'heet434'
-mongo_uri = ""
-mongo_db = ""
-mongo_collection = ""
+mongo_uri = "mongodb+srv://hiheet04:hiheet04@cluster0.y39du.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongo_db = "iot"
+mongo_collection = "water_quantity"
